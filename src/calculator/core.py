9d13a702be3a4e86e5cf5efe2761598b6a2e7c9f
ftk1000 <khafizov.farid@gmail.com>
2026-09-03 00:01:31 -0500

    Introduce bug in subtract (intentional)

--- src/calculator/core.py
+++ src/calculator/core.py
@@ -6,7 +6,7 @@ def add(a: float, b: float) -> float:
 
 
 def subtract(a: float, b: float) -> float:
-    return a - b
+    return a + b  # BUG: should be minus
 
 
 def divide(a: float, b: float) -> float:
